351a6d196994ef8d00adfa5b9e18a4d6b66fef1d
GHz122 <gui.denez56@gmailcom>
2021-06-25 21:03:03 -0300

    atualizacao 25-06-21

--- InterV1/estrutura/verificacao-sessao-usuario.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-
-    if (!isset($_SESSION['ID'])) {
-        unset($_SESSION['ID']);
-        session_destroy();
-        header("Location: ../paginas/logPage.php");
-    }
-?>
--- InterV1/paginas/DAO/sair.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['ID']);
-    session_destroy();
-    header("Location: ../index.php");
-?>
